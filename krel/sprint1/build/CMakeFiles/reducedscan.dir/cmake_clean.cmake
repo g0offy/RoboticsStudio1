7d5ca37384ea79d71f5028e943b3bd419c1dec89
Karl Cincollagas <karl.cincollagas@gmail.com>
2024-08-22 19:42:50 +1000

    krel sprint1

---
+++ krel/sprint1/build/CMakeFiles/reducedscan.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reducedscan.dir/src/reduced_scan.cpp.o"
+  "CMakeFiles/reducedscan.dir/src/reduced_scan.cpp.o.d"
+  "reducedscan"
+  "reducedscan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reducedscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ krel/sprint1/src/reduced_scan.cpp
@@ -0,0 +1,59 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+class LaserScanModifier : public rclcpp::Node
+{
+public:
+    LaserScanModifier()
+    : Node("laser_scan_modifier")
+    {
+        // Subscription to /scan topic
+        sub1_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "/scan", 10, std::bind(&LaserScanModifier::callback, this, std::placeholders::_1));
+        // Publisher created for /filtered_scan topic
+        pub1_ = this->create_publisher<sensor_msgs::msg::LaserScan>("filtered_scan", 10);
+        // Step size for filtering
+        n_ = 5; // filtered by nth value, displays every nth point
+    }
+
+private:
+    void callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const
+    {
+        auto modifiedMsg = sensor_msgs::msg::LaserScan();
+        modifiedMsg.header = msg->header;
+        modifiedMsg.angle_min = msg->angle_min;
+        modifiedMsg.angle_max = msg->angle_max;
+        modifiedMsg.angle_increment = msg->angle_increment * n_;
+        modifiedMsg.time_increment = msg->time_increment * n_;
+        modifiedMsg.scan_time = msg->scan_time;
+        modifiedMsg.range_min = msg->range_min;
+        modifiedMsg.range_max = msg->range_max;
+
+        // Filter ranges
+        for (size_t i = 0; i < msg->ranges.size(); i += n_) {
+            modifiedMsg.ranges.push_back(msg->ranges[i]);
+            RCLCPP_INFO(this->get_logger(), "Adding range index: %zu", i);
+        }
+
+        // Filter intensities if available
+        if (!msg->intensities.empty()) {
+            for (size_t i = 0; i < msg->intensities.size(); i += n_) {
+                modifiedMsg.intensities.push_back(msg->intensities[i]);
+                RCLCPP_INFO(this->get_logger(), "Adding intensity index: %zu", i);
+            }
+        }
+
+        // Publish the filtered message
+        pub1_->publish(modifiedMsg);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub1_;
+    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr pub1_;
+    size_t n_;
+};
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<LaserScanModifier>());
+    rclcpp::shutdown();
+    return 0;
+}
