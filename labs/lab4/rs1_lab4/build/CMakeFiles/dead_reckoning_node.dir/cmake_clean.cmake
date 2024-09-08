f2d80c5a9efbcc417395fb6e4e2ee11dbdc52f3b
Karl Cincollagas <karl.cincollagas@gmail.com>
2024-09-08 15:57:40 +1000

    added lab 4

---
+++ labs/lab4/rs1_lab4/build/CMakeFiles/dead_reckoning_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dead_reckoning_node.dir/src/dead_reckoning_node.cpp.o"
+  "CMakeFiles/dead_reckoning_node.dir/src/dead_reckoning_node.cpp.o.d"
+  "dead_reckoning_node"
+  "dead_reckoning_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dead_reckoning_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ labs/lab4/rs1_lab4/build/CMakeFiles/dead_reckoning_node.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dead_reckoning_node.
+# This may be replaced when dependencies are built.
